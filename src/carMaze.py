17d0a43275eba23dbe334d215b39a3b958321cd4
Lordpianop <ImpossibleKirby@outlook.com>
2026-04-18 14:49:11 -0500

    Added a clock timer, created the base for loop to run application

--- src/carMaze.py
+++ src/carMaze.py
@@ -7,3 +7,11 @@ def main():
 
     resolution = (titleSize * gridWidth, titleSize * gridHeight)
     screen = pygame.display.set_mode(resolution)
+
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
