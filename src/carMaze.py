2ea719c08bbda1079ba174e32fd4a95cca53ff2b
Lordpianop <ImpossibleKirby@outlook.com>
2026-04-18 14:47:31 -0500

    Creating a custom resolution for the display screen.

--- src/carMaze.py
+++ src/carMaze.py
@@ -1,3 +1,9 @@
 import pygame
 
 def main():
+    titleSize = 40
+    gridWidth = 15
+    gridHeight = 15
+
+    resolution = (titleSize * gridWidth, titleSize * gridHeight)
+    screen = pygame.display.set_mode(resolution)
